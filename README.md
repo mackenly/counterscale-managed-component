f52beb6987bbb86ebe1f781a17db26090f0b41a8
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-01-25 15:55:03 -0500

    Trying packaging via codespace

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # Counterscale Managed Component
 
 ## Documentation
-
-Managed Components docs are published at **https://managedcomponents.dev** .
-
-Find out more about Managed Components [here](https://blog.cloudflare.com/zaraz-open-source-managed-components-and-webcm/) for inspiration and motivation details.
+ 
+ 
 
 [![Released under the Apache license.](https://img.shields.io/badge/license-apache-blue.svg)](./LICENSE)
 [![PRs welcome!](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](./CONTRIBUTING.md)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 
-## 🚀 Quickstart local dev environment
-
-1. Make sure you're running node version >=18.
-2. Install dependencies with `npm i`
-3. Run unit test watcher with `npm run test:dev`
+ 
 
 ## ⚙️ Tool Settings
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
 import { ComponentSettings, Manager } from '@managed-components/types'
 
+interface Params {
+  sid: string // site id
+  h: string // host
+  p: string // pathname
+  r: string // referer
+}
+
 export default async function (manager: Manager, _settings: ComponentSettings) {
-  manager.addEventListener('pageview', event => {
+  manager.addEventListener('pageview', async event => {
+    const { client } = event
     console.log('Hello server!')
+
+    // last time seen for session and new visitor calculation
+    const lastTimeSeen: string =
+      client.get('if_modified_since') || Date.now().toString()
+    const currentTime = Date.now()
+    client.set('if_modified_since', currentTime.toString(), {
+      scope: 'infinite',
+    })
+
+    // site id and api hostname
+    const { siteId, hostname } = _settings
+
+    const paramData: Params = {
+      sid: siteId || client.url.hostname,
+      h: client.url.host,
+      p: client.url.pathname,
+      r: client.referer, // notice the different spelling
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const params = new URLSearchParams(paramData as any)
+
+    await manager.fetch(`${hostname}/collect?${params.toString()}`, {
+      method: 'POST',
+      headers: {
+        'user-agent': client.userAgent,
+        'if-modified-since': lastTimeSeen,
+      },
+    })
+
     event.client.execute("console.log('Hello browser')")
   })
 }

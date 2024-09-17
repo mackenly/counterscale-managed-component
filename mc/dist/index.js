697ddb7dc4b1e0a19041a3e4a7dae07f29f26b2c
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-09-17 22:47:13 +0000

    Upgrades eslint config

--- mc/dist/index.js
+++ mc/dist/index.js
@@ -1,1 +1,32 @@
-async function l(r,a){r.addEventListener("pageview",async i=>{let{client:e}=i,s=e.get("if_modified_since")||"",o=new Date().toUTCString();e.set("if_modified_since",o.toString(),{scope:"infinite"});let c={sid:a.siteId||e.url.hostname,h:e.url.host,p:e.url.pathname,r:e.referer},g=new URLSearchParams(c),n=a.apiBaseUrl;n.endsWith("/")||(n+="/"),n+=`collect?${g.toString()}`,await r?.ext?.env.counterscale_worker.fetch(`${n}`,{method:"POST",headers:{"user-agent":e.userAgent,"if-modified-since":s},cf:{country:i.payload.country}}).then(t=>t.text()).then(t=>{console.log("text",t)}).catch(t=>{console.log("err",t)})})}export{l as default};
+async function l(r, a) {
+	r.addEventListener('pageview', async i => {
+		let { client: e } = i,
+			s = e.get('if_modified_since') || '',
+			o = new Date().toUTCString();
+		e.set('if_modified_since', o.toString(), { scope: 'infinite' });
+		let c = {
+				sid: a.siteId || e.url.hostname,
+				h: e.url.host,
+				p: e.url.pathname,
+				r: e.referer,
+			},
+			g = new URLSearchParams(c),
+			n = a.apiBaseUrl;
+		n.endsWith('/') || (n += '/'),
+			(n += `collect?${g.toString()}`),
+			await r?.ext?.env.counterscale_worker
+				.fetch(`${n}`, {
+					method: 'POST',
+					headers: { 'user-agent': e.userAgent, 'if-modified-since': s },
+					cf: { country: i.payload.country },
+				})
+				.then(t => t.text())
+				.then(t => {
+					console.log('text', t);
+				})
+				.catch(t => {
+					console.log('err', t);
+				});
+	});
+}
+export { l as default };
--- mc/src/index.ts
+++ mc/src/index.ts
@@ -1,52 +1,57 @@
 import {
 	ComponentSettings,
 	Manager as OriginalManager,
-} from '@managed-components/types'
+} from '@managed-components/types';
+import {
+	RequestInit,
+	Response,
+	URLSearchParams,
+} from '@cloudflare/workers-types';
 
 interface Manager extends OriginalManager {
 	ext: {
 		env: {
 			counterscale_worker: {
-				fetch: (url: string, init?: RequestInit) => Promise<Response>
-			}
-		}
-	}
+				fetch: (url: string, init?: RequestInit) => Promise<Response>;
+			};
+		};
+	};
 }
 
 interface Params {
-	sid: string // site id
-	h: string // host
-	p: string // pathname
-	r: string // referer
+	sid: string; // site id
+	h: string; // host
+	p: string; // pathname
+	r: string; // referer
 }
 
 export default async function (manager: Manager, settings: ComponentSettings) {
 	manager.addEventListener('pageview', async event => {
-		const { client } = event
+		const { client } = event;
 
 		// last time seen for session and new visitor calculation
-		const lastTimeSeen: string = client.get('if_modified_since') || ''
-		const currentTime = new Date().toUTCString()
+		const lastTimeSeen: string = client.get('if_modified_since') || '';
+		const currentTime = new Date().toUTCString();
 		client.set('if_modified_since', currentTime.toString(), {
 			scope: 'infinite',
-		})
+		});
 
 		const paramData: Params = {
 			sid: settings.siteId || client.url.hostname,
 			h: client.url.host,
 			p: client.url.pathname,
 			r: client.referer, // notice the different spelling
-		}
+		};
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const params = new URLSearchParams(paramData as any)
+		const params = new URLSearchParams(paramData as any);
 
 		// make sure apiBaseUrl ends with a slash
-		let apiUrl = settings.apiBaseUrl
+		let apiUrl = settings.apiBaseUrl;
 		if (!apiUrl.endsWith('/')) {
-			apiUrl += '/'
+			apiUrl += '/';
 		}
-		apiUrl += `collect?${params.toString()}`
+		apiUrl += `collect?${params.toString()}`;
 
 		await manager?.ext?.env.counterscale_worker
 			.fetch(`${apiUrl}`, {
@@ -61,10 +66,10 @@ export default async function (manager: Manager, settings: ComponentSettings) {
 			} as RequestInit) // Add type assertion to include 'cf' property
 			.then(res => res.text())
 			.then(text => {
-				console.log('text', text)
+				console.log('text', text);
 			})
 			.catch(err => {
-				console.log('err', err)
-			})
-	})
+				console.log('err', err);
+			});
+	});
 }
